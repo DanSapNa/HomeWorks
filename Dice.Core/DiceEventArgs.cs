f26a0f4e009af0a64e1dbb97ff6e20f73a1e90b2
Oksana Makarenko <oksana.makarenko@rambler.ru>
2018-03-14 12:21:22 +0200

    Added initial version

---
+++ Dice.Core/DiceEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dice.Core
+{
+    public class DiceEventArgs : EventArgs
+    {
+        public int FirstDiceSide { get; private set; }
+        public int SecondDiceSide { get; private set; }
+
+        public DiceEventArgs(int firstDiceSide, int secondDiceSide)
+        {
+            FirstDiceSide = firstDiceSide;
+            SecondDiceSide = secondDiceSide;
+        }
+    }
+}
---
+++ Dice.Core/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Dice.Core
+{
+    public class Player
+    {
+        private int _firstDiceSide;
+        private int _secondDiceSide;
+
+        public event EventHandler<DiceEventArgs> Throwed;
+
+        public Player()
+        {
+        }
+
+        public void ThrowDice()
+        {
+            Random random = new Random();
+            this._firstDiceSide = random.Next(4, 7);
+            this._secondDiceSide = random.Next(6, 7);
+
+            Console.WriteLine("{0}::{1}", _firstDiceSide, _secondDiceSide);
+
+            if (_firstDiceSide == 6 & _secondDiceSide == 6)
+            {
+                OnThrowed();
+            }
+        }
+
+        private void OnThrowed()
+        {
+            if (Throwed != null)
+            {
+                Throwed(this, new DiceEventArgs(_firstDiceSide, _secondDiceSide));
+            }
+        }
+    }
+}
