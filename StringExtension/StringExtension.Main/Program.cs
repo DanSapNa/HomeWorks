ec01fac6db400137e6fb1d2aa5cf78ef24c18ec5
Oksana Makarenko <oksana.makarenko@rambler.ru>
2018-04-02 11:56:09 +0300

    Added String Extension homework

---
+++ StringExtension/StringExtension.Main/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtension.Main
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a string of integers: ");
+            var inputString = Console.ReadLine();
+
+            int[] stringNumbers = inputString.GetNumbers();
+
+            foreach (int number in stringNumbers)
+            {
+                Console.WriteLine($"\t{number}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StringExtension/StringExtension/StringExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtension
+{
+    public static class StringExtension
+    {
+        public static int[] GetNumbers(this string inputString)
+        {
+            try
+            {
+                return Array.ConvertAll(inputString.Split(','), int.Parse);
+            }
+            catch (FormatException ex)
+            {
+                throw new FormatException($"Wrong format. String should contain integer value separated with coma. Exception:: {ex.Message}");
+            }
+        }
+    }
+}
