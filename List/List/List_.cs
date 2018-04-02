67f5c1057510e0b78e82ec210cf71130a92287c3
Oksana Makarenko <oksana.makarenko@rambler.ru>
2018-04-02 11:48:26 +0300

    Added List homework

---
+++ List/List/List_.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    public class List_
+    {
+        private const int InitialSize = 4;
+        private const int ExtendIn = 2;
+        public object[] _array;
+        private int _count;
+        public int Count { get { return _count; } }
+
+        public object this[int index]
+        {
+            get
+            {
+                if (index < _count)
+                {
+                    return _array[index];
+                }
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        public List_()
+        {
+            _array = new object[InitialSize];
+        }
+
+        public void Add(object objectToAdd)
+        {
+            if (objectToAdd == null)
+            {
+                throw new ArgumentNullException("objectToAdd");
+            }
+
+            bool added = false;
+
+            TryAddToFirstEmptyPosition(objectToAdd, out added);
+
+            if (!added)
+            {
+                ExtendArray();
+                AddToLastPosition(objectToAdd);
+            }
+
+            _count++;
+        }
+
+        private void TryAddToFirstEmptyPosition(object objectToAdd, out bool added)
+        {
+            added = false;
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i] == null)
+                {
+                    _array[i] = objectToAdd;
+                    added = true;
+                    return;
+                }
+            }
+        }
+
+        private void ExtendArray()
+        {
+            var newArray = new object[_array.Length * ExtendIn];
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                newArray[i] = _array[i];
+            }
+
+            _array = newArray;
+        }
+
+        private void AddToLastPosition(object objectToAdd)
+        {
+            _array[_count] = objectToAdd;
+        }
+
+        public void Insert(int index, object objectToSet)
+        {
+            if (index <= _count)
+            {
+                if (_count == _array.Length)
+                {
+                    ExtendArray();
+                }
+
+                for (int i = _array.Length - 2; i > index - 1; i--)
+                {
+                    _array[i + 1] = _array[i];
+                }
+                _array[index] = objectToSet;
+                _count++;
+            }
+            else
+            {
+                throw new Exception("Index is greater that array length.");
+            }
+        }
+
+        public void Remove(object objectToRemove)
+        {
+            int indexRemoved = -1;
+
+            for (int i = 0; i < _count; i++)
+            {
+                if (_array[i].Equals(objectToRemove))
+                {
+                    _array[i] = null;
+                    indexRemoved = i;
+                }
+            }
+
+            if (indexRemoved == -1)
+            {
+                return;
+            }
+
+            for (int i = indexRemoved; i < _count - 1; i++)
+            {
+                _array[i] = _array[i + 1];
+            }
+
+            _count--;
+            _array[_count] = null;
+        }
+
+        public void RemoveAt(int indexToRemove)
+        {
+            if (indexToRemove < _count)
+            {
+                _array[indexToRemove] = null;
+
+                for (int i = indexToRemove; i < _count - 1; i++)
+                {
+                    _array[i] = _array[i + 1];
+                }
+
+                _count--;
+                _array[_count] = null;
+                return;
+            }
+            throw new IndexOutOfRangeException();
+        }
+
+        public void Clear()
+        {
+            if (_count > 0)
+            {
+                _array = new object[InitialSize];
+                _count = 0;
+            }
+        }
+
+        public bool CheckArrayItemIsNull(int index)
+        {
+            if (index > _array.Length - 1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return _array[index] == null;
+        }
+    }
+}
